00f016bca99085dd190961e0c109d2aecbb7db25
MarittyaKeu <MarittyaKeu@gmail.com>
2018-11-09 13:44:06 -0500

    First commit

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: '5926303092-ujs1hm0td2r7qav3dvq2gnsr3mc0rnkt.apps.googleusercontent.com',
+        clientSecret: 'z67xTOFDRcykct0UEOyMi9-9'
+    },
+
+    mongodb: {
+        dbURI: 'mongodb://mike:test@ds151943.mlab.com:51943/oauth-test'
+    }
+};
